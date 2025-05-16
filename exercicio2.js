3eb949e4165e1d2d48008f91068ffe2283818b3a
Pierre <pipierre4321@gmail.com>
2025-05-15 21:48:49 -0300

    Exercícios 2

---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+/* Exe 1 Crie variáveis que armazenem o valor do saldo, de depósito e de um saque de uma conta bancária */
+let saldo = 1000;
+let deposito = 300;
+let saque = 200;
+let operacao = saldo + deposito - saque;
+console.log(operacao);
+
+/* Exe 2 Crie uma variável numérica com um valor utilizar um operador para verificar se é impar ou par */
+let num = 3;
+let parImpar = num % 2 === 0 ? "Par" : "Ímpar";
+
+/*
