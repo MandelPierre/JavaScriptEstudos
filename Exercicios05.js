61b98dc48cc49b61ed1e41a61a2b08acc7d9bc92
Pierre <pipierre4321@gmail.com>
2025-05-16 20:28:37 -0300

    alteração nas atividades

--- Exercicios05.js
+++ Exercicios05.js
@@ -4,8 +4,8 @@
 let salario1 = 2200;
 let salario2 = 3000;
 let somaSalario = salario1 + salario2;
-let salarioMinimo = 1525
+let salarioMinimo = 1525;
 
 let quantidadeSalariosMinimos = somaSalario / salarioMinimo;
-console.log("A soma dos salários é: R$ " + somaSalario)
+console.log("A soma dos salários é: R$ " + somaSalario);
 console.log("Isso equivale a " + quantidadeSalariosMinimos + " salários mínimos.");
---
+++ Exercicios06.js
@@ -0,0 +1,7 @@
+/*Faça um algoritmo que imprima na tela com um reajuste de 5% nos seus salários.*/
+let salarioJuntos = 5200;
+let percentual = 5;
+let reajuste = salarioJuntos * percentual / 100;
+let novoSalario = salarioJuntos + reajuste;
+
+console.log("Com o reajuste o salário ficou R$", novoSalario);
