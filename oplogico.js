c4ba8b66f8fd59bfdf38ec2603a953ce5e268219
Pierre <pipierre4321@gmail.com>
2025-05-15 21:33:31 -0300

    Op Lógicos

--- oplogico.js
+++ oplogico.js
@@ -1,6 +1,15 @@
 let idade = 18;
 let temCarteira = true;
+let consumiuBebida = true;
 
 if(idade >= 18 && temCarteira) {
-    console.log("Você pode dirigir!")
+    console.log("Você pode dirigir!");
+}
+
+if(idade < 18 || !temCarteira) {
+    console.log("Você não poder dirigir!");
+}
+
+if(consumiuBebida) {
+    console.log("Você não pode dirigir no momento!");
 }
