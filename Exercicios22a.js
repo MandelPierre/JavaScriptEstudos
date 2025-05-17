0642c011ed11d6c5032376212df65b5c1f4e9b42
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2025-05-17 16:17:50 -0300

    Create Exercicios22a.js

---
+++ Exercicios22a.js
@@ -0,0 +1,8 @@
+/*Faça um algoritmo que leia dois valores inteiros A e B, imprima na tela o quociente e o
+resto da divisão inteira entre eles.*/
+let A = 5;
+let B = 3;
+let divisao = A / B;
+let resto = A % B;
+
+console.log(``)
