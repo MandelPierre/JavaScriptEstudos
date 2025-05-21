cce61f0aa6550ab07b896a8c055208bb0d6cc183
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2025-05-21 19:51:46 -0300

    Update Exercicios25.js

--- Exercicios25.js
+++ Exercicios25.js
@@ -3,13 +3,13 @@ let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let somaPares = 0;
 let somaImpares = 0;
 
-    for (let i = 0; i < numeros.length; i++) {
-        if (numeros[i] % 2 === 0) {
-            somaPares += numeros[i];
-        } else {
-            somaImpares += numeros[i];
-        }
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+        somaPares += numeros[i];
+    } else {
+        somaImpares += numeros[i];
     }
+}
 
 console.log(`Soma dos números pares: ${somaPares}`);
 console.log(`Soma dos números ímpares: ${somaImpares}`);
