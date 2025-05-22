2ba1ffb564736d79760165e98c68839643afa104
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2025-05-21 21:25:43 -0300

    Create Exercicios30.js

---
+++ Exercicios30.js
@@ -0,0 +1,6 @@
+/**Faça um algoritmo que receba um número decimal e converta para binário, imprimindo na tela o resultado.*/
+let numeroDecimal = 25;
+
+let numeroBinario = numeroDecimal.toString(2);
+
+console.log(`O número ${numeroDecimal} em binário é: ${numeroBinario}`);
