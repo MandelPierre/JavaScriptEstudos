5fce68a6fa0c5985f4f90741135fee31f1784e74
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2025-06-15 19:36:33 -0300

    criado Exercicio07

---
+++ Exercicios For Arrays/Exercicio07.js
@@ -0,0 +1,6 @@
+//Utilize o método forEach para imprimir cada elemento de um array juntamente com seu índice.
+const num = [5, 3, 9, 12, 0, 8];
+
+num.forEach((numero, indice) => {
+    console.log(`O número do índice ${indice} é: ${numero}`);
+});
